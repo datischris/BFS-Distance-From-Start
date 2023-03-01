37ef3517a972824b3ac14920cc2ab1c1c717fd9c
datischris <christopher.varghese@hotmail.com>
2023-03-01 12:42:13 -0500

    added comments

--- Solution.py
+++ Solution.py
@@ -7,19 +7,19 @@ class Solution:
         self.graph = graph
 
     def output_distances(self):
-        queue = deque([self.start_node])
-        visited = set([self.start_node])
-        distarray = [-1] * (list(self.graph.keys())[-1]+1)
+        queue = deque([self.start_node])                     # init queue
+        visited = set([self.start_node])                     # init visited set
+        distarray = [-1] * (list(self.graph.keys())[-1]+1)   # init distance array with -1 for nodes not visited
 
-        distarray[self.start_node] = 0
+        distarray[self.start_node] = 0                       # updated start node distance with 0
 
         while queue:
-            pnode = queue.popleft()
+            pnode = queue.popleft()                          # pop first element in queue
             graph = self.graph[pnode]
             for node in graph:
-                if node not in visited:
-                    queue.append(node)
-                    visited.add(node)
-                    distarray[node] = (distarray[pnode] + 1)
+                if node not in visited:                      # if cc of node hasn't been visited 
+                    queue.append(node)                       # append to queue
+                    visited.add(node)                        # add to visited set
+                    distarray[node] = (distarray[pnode] + 1) # update distance with parent nodes distance + 1
 
         return distarray
